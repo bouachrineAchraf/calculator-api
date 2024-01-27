528e45aa8b2cdf74c18eafbdf67be207f44fa8ed
bouachrine <bouachrineachraf04@gmail.com>
2024-01-28 00:53:08 +0100

    feat: Set up FastAPI application with database integration

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from database import  engine, SessionLocal
+import models
+from pydantic import BaseModel
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def create_calculation_table():
+    models.Base.metadata.create_all(bind=engine)
+
+class CalculationBase(BaseModel):
+    expression: str
+    result: str
+
+class CalculationCreate(CalculationBase):
+    result: str
 
 @app.get('/')
 def home():
